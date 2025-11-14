e1be045d035f71580e37c9641eac85797bcb6092
WolbachAuth0 <aaron.wolbach@auth0.com>
2025-11-14 09:13:55 -0500

    Add custom phone provider script.

---
+++ tenant/custom-phone-provider.js
@@ -0,0 +1,20 @@
+/**
+* Handler to be executed while sending a phone notification*
+* @param {Event} event - Details about the user and the context in which they are logging in.
+* @param {CustomPhoneProviderAPI} api - Methods and utilities to help change the behavior of sending a phone notification.
+*/
+exports.onExecuteCustomPhoneProvider = async (event, api) => {
+  console.log('sms messager action');
+  const axios = require('axios');
+  const url = 'https://otp.awolcustomdemos.com/api/messages'
+  const now = new Date();
+  const body = {
+      recipient: event.notification.recipient,
+      body: event.notification.as_text,
+      sender:"+1 234 567 8910",
+      timestamp:  now.getTime()
+  }
+  const result = await axios.post(url, body);
+  console.log('result', result)
+  return;
+};
