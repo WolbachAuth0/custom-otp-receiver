8ea7c47e9c23188b5ce12c6113a283338c60c9c7
WolbachAuth0 <aaron.wolbach@auth0.com>
2026-06-08 17:21:44 -0400

    working commit

--- scripts/send-message.js
+++ scripts/send-message.js
@@ -10,28 +10,41 @@ main();
 async function main () {
   try {
     const tokenResponse = await fetchToken();
-    console.log(tokenResponse.data)
-
+    const accessToken = tokenResponse.data.access_token;
+    const response = await sendMessage(accessToken);
+    console.log(response.data)
   } catch (error) {
     console.log(error);
   }
 }
 
 async function fetchToken () {
-  const body = {
-    client_id: process.env.M2M_CLIENT_ID,
-    client_secret: process.env.M2M_CLIENT_SECRET,
+
+  const token = {
+    data: {
+      "access_token": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InZ4ZFpwVHdqT0MtZXZsRVVKWFlGOCJ9.eyJpc3MiOiJodHRwczovL2xvZ2luLm90cC5hd29sY3VzdG9tZGVtb3MuY29tLyIsInN1YiI6IktHYUxIeThGTnJCeTJOcVBzYUFRdUVhaXdqeVp5enRCQGNsaWVudHMiLCJhdWQiOiJodHRwczovL290cC5hd29sY3VzdG9tZGVtb3MuY29tL2FwaSIsImlhdCI6MTc4MDkzNjQxNiwiZXhwIjoxNzgxMDIyODE2LCJzY29wZSI6InJlYWQ6bWVzc2FnZXMgZGVsZXRlOm1lc3NhZ2VzIGNyZWF0ZTptZXNzYWdlIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIiwiYXpwIjoiS0dhTEh5OEZOckJ5Mk5xUHNhQVF1RWFpd2p5Wnl6dEIifQ.gErHOk1myRnfD4l0XaxoC9KQq2ImDV4INLrvXwoFdsvwTfB7c0QTYxsbHK0cS9LM1EDFnGVPkAv0eTh2HEtjw0Wq0M4JrqBLdsNzIlK1hfUKWvycn3tPkK-T3aABC6AYXB4LKAtleeQlvcPrN0hpZ2fWRXjfre32FR3YMkXc8Im3lnJWBOwmsebMA55P7vB_V2L3lpMv7VarDB8m1Jrk8cTvK3o9JKjArQ6bOvJ2jzfQM8PiCQGACF-STO4X_eVTFJRyKwM0RkEJMria6vw26hgNb18Rj7wlrD7NCL_YpbCM0OSVlidnllupcMipqDtSGh_8BNCyqwqjC3v9c1bmYA",
+      "scope": "read:messages delete:messages create:message",
+      "expires_in": 86400,
+      "token_type": "Bearer",
+      "fromCache": true
+    }
   }
-  const options = {
-    timeout: 1000,
-    headers: [
-      { 'Content-Type': 'application/json' },
-      { 'Accept': 'application/json' }
-    ] 
-  }
-  console.log('Fetching access token.')
-  const response = await axios.post(`${baseURL}/oauth/token`, body, options)
-  return response.data
+  return token
+
+  // const body = {
+  //   client_id: process.env.M2M_CLIENT_ID,
+  //   client_secret: process.env.M2M_CLIENT_SECRET,
+  // }
+  // const options = {
+  //   timeout: 1000,
+  //   headers: [
+  //     { 'Content-Type': 'application/json' },
+  //     { 'Accept': 'application/json' }
+  //   ] 
+  // }
+  // console.log('Fetching access token.')
+  // const response = await axios.post(`${baseURL}/oauth/token`, body, options)
+  // return response.data
 }
 
 async function sendMessage (accessToken) {
@@ -46,7 +59,7 @@ async function sendMessage (accessToken) {
   const options = {
     timeout: 1000,
     headers :[
-      { Authorization: `Bearer ${accessToken}`},
+      { 'Authorization': `Bearer ${accessToken}`},
       { 'Content-Type': 'application/json' },
       { 'Accept': 'application/json' }
     ]
