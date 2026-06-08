0865195e9e27e06c0bd2e6d02367c3b59a7d4442
WolbachAuth0 <aaron.wolbach@auth0.com>
2026-06-08 17:24:17 -0400

    fix headers

--- scripts/send-message.js
+++ scripts/send-message.js
@@ -19,32 +19,20 @@ async function main () {
 }
 
 async function fetchToken () {
-
-  const token = {
-    data: {
-      "access_token": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InZ4ZFpwVHdqT0MtZXZsRVVKWFlGOCJ9.eyJpc3MiOiJodHRwczovL2xvZ2luLm90cC5hd29sY3VzdG9tZGVtb3MuY29tLyIsInN1YiI6IktHYUxIeThGTnJCeTJOcVBzYUFRdUVhaXdqeVp5enRCQGNsaWVudHMiLCJhdWQiOiJodHRwczovL290cC5hd29sY3VzdG9tZGVtb3MuY29tL2FwaSIsImlhdCI6MTc4MDkzNjQxNiwiZXhwIjoxNzgxMDIyODE2LCJzY29wZSI6InJlYWQ6bWVzc2FnZXMgZGVsZXRlOm1lc3NhZ2VzIGNyZWF0ZTptZXNzYWdlIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIiwiYXpwIjoiS0dhTEh5OEZOckJ5Mk5xUHNhQVF1RWFpd2p5Wnl6dEIifQ.gErHOk1myRnfD4l0XaxoC9KQq2ImDV4INLrvXwoFdsvwTfB7c0QTYxsbHK0cS9LM1EDFnGVPkAv0eTh2HEtjw0Wq0M4JrqBLdsNzIlK1hfUKWvycn3tPkK-T3aABC6AYXB4LKAtleeQlvcPrN0hpZ2fWRXjfre32FR3YMkXc8Im3lnJWBOwmsebMA55P7vB_V2L3lpMv7VarDB8m1Jrk8cTvK3o9JKjArQ6bOvJ2jzfQM8PiCQGACF-STO4X_eVTFJRyKwM0RkEJMria6vw26hgNb18Rj7wlrD7NCL_YpbCM0OSVlidnllupcMipqDtSGh_8BNCyqwqjC3v9c1bmYA",
-      "scope": "read:messages delete:messages create:message",
-      "expires_in": 86400,
-      "token_type": "Bearer",
-      "fromCache": true
+  const body = {
+    client_id: process.env.M2M_CLIENT_ID,
+    client_secret: process.env.M2M_CLIENT_SECRET,
+  }
+  const options = {
+    timeout: 1000,
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
     }
   }
-  return token
-
-  // const body = {
-  //   client_id: process.env.M2M_CLIENT_ID,
-  //   client_secret: process.env.M2M_CLIENT_SECRET,
-  // }
-  // const options = {
-  //   timeout: 1000,
-  //   headers: [
-  //     { 'Content-Type': 'application/json' },
-  //     { 'Accept': 'application/json' }
-  //   ] 
-  // }
-  // console.log('Fetching access token.')
-  // const response = await axios.post(`${baseURL}/oauth/token`, body, options)
-  // return response.data
+  console.log('Fetching access token.')
+  const response = await axios.post(`${baseURL}/oauth/token`, body, options)
+  return response.data
 }
 
 async function sendMessage (accessToken) {
@@ -58,11 +46,11 @@ async function sendMessage (accessToken) {
   }
   const options = {
     timeout: 1000,
-    headers :[
-      { 'Authorization': `Bearer ${accessToken}`},
-      { 'Content-Type': 'application/json' },
-      { 'Accept': 'application/json' }
-    ]
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
   }
   console.log('sending sms messager test message');
   const response = await axios.post(`${baseURL}/api/messages`, body, options);
