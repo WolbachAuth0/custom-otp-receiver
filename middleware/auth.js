5d04a34111089eb01e3c553d27429318414c5388
WolbachAuth0 <aaron.wolbach@auth0.com>
2026-06-10 14:08:40 -0400

    accept custom and tenant domain issuers

--- middleware/auth.js
+++ middleware/auth.js
@@ -16,7 +16,10 @@ const verifyJWT = jwt({
     jwksUri: `https://${process.env.AUTH0_CUSTOM_DOMAIN}/.well-known/jwks.json`
   }),
   audience: process.env.AUDIENCE,
-  issuer: `https://${process.env.AUTH0_CUSTOM_DOMAIN}/`,
+  issuer: [
+    `https://${process.env.AUTH0_CUSTOM_DOMAIN}/`,
+    `https://${process.env.AUTH0_DOMAIN}/`
+  ],
   algorithms: ['RS256']
 })
 
